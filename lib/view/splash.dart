952335ea9184ce8b5a9e6236f717feecbddaceae
iremkryt <103690738+iremkryt@users.noreply.github.com>
2023-04-17 21:51:48 +0300

    Created splashScreen

---
+++ lib/view/splash.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+
+class Splash extends StatefulWidget {
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
