2c0a67d4abcff3adb27c6a22cbe3756cdb0c5cd3
iremkryt <103690738+iremkryt@users.noreply.github.com>
2023-04-20 22:54:35 +0300

    Updated main

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:ecommerce_design/view/splash.dart';
 import 'package:flutter/material.dart';
+import 'package:grock/grock.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(ProviderScope(child: MyApp()));
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    @override
     return MaterialApp(
       title: 'Online Shopping',
       debugShowCheckedModeBanner: false,
+      navigatorKey: Grock.navigationKey,
+      scaffoldMessengerKey: Grock.snackbarMessengerKey,
       home: Splash(),
     );
   }
